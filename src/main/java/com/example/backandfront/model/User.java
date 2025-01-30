9759730b582f46c32a327b48a2f488ed2d4833c7
Alberto <albertojunior848423803@gmail.com>
2025-01-31 00:51:22 +0300

    first commit

--- src/main/java/com/example/backandfront/model/User.java
+++ src/main/java/com/example/backandfront/model/User.java
@@ -1,8 +1,6 @@
 package com.example.backandfront.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import javax.swing.*;
 
@@ -11,6 +9,7 @@ import javax.swing.*;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
     private String name;
     private int age;
@@ -24,4 +23,27 @@ public class User {
         this.age = age;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
---
+++ src/main/java/com/example/backandfront/service/UserService.java
@@ -0,0 +1,29 @@
+package com.example.backandfront.service;
+
+import com.example.backandfront.model.User;
+import com.example.backandfront.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User addUser(User user){
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllStudents(){
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getStudentById(String id){
+        return userRepository.findById(id);
+    }
+}
