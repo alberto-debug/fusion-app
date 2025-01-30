ee01a239c44671fc715323c65c7bd78a2af1f76b
Alberto <albertojunior848423803@gmail.com>
2025-01-31 00:29:56 +0300

    first commit

--- src/main/java/com/example/backandfront/model/User.java
+++ src/main/java/com/example/backandfront/model/User.java
@@ -1,4 +1,17 @@
 package com.example.backandfront.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "test")
 public class User {
+
+    @Id
+    private String id;
+    private String name;
+    private int age;
+
+
 }
