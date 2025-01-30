f23be49eaef42eea5eab701d514363897408d893
Alberto <albertojunior848423803@gmail.com>
2025-01-30 23:53:12 +0300

    first commit

---
+++ src/test/java/com/example/backandfront/BackandfrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backandfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackandfrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
