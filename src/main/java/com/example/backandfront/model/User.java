9bcf76dc414a0fba268c2c60486a3ed90b89a5eb
Alberto <albertojunior848423803@gmail.com>
2025-01-31 00:33:25 +0300

    first commit

--- src/main/java/com/example/backandfront/model/User.java
+++ src/main/java/com/example/backandfront/model/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+import javax.swing.*;
+
 @Entity
 @Table(name = "test")
 public class User {
@@ -13,5 +15,13 @@ public class User {
     private String name;
     private int age;
 
+    public User(){
+
+    }
+
+    public User(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
 
 }
